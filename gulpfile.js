0cb1e7cc89ab8ea53f89b91f17e1589d9c56f276
Rémi Becheras <rbecheras@sirap.fr>
2016-12-15 23:50:53 +0100

    create the 'lint' task w/ the standard linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,14 @@
 var gulp = require('gulp')
+var linter = require('gulp-standard-bundle').linter
 var mocha = require('gulp-mocha')
+
+gulp.task('lint', function () {
+  gulp.src(['./lib/**/*.js', './tests/**/*.spec.js'])
+  .pipe(linter())
+  .pipe(linter.reporter('default', {
+    breakOnError: true
+  }))
+})
 
 gulp.task('test', function () {
   gulp.src('./tests/connect-sequence.spec.js').pipe(mocha())
