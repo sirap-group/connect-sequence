03d80ac575d4df32b4dc8e04a70552f873bc342e
Rémi Becheras <rbecheras@sirap.fr>
2016-12-16 03:12:15 +0100

    create bump task

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp')
 var linter = require('gulp-standard-bundle').linter
 var mocha = require('gulp-mocha')
-var sequence = require('run-sequence')
+var bump = require('gulp-bump')
+
+// var sequence = require('run-sequence')
+var argv = require('yargs').argv
 
 gulp.task('lint', function (done) {
   gulp.src(['./gulpfile.js', './lib/**/*.js', './tests/**/*.spec.js'])
@@ -12,10 +15,22 @@ gulp.task('lint', function (done) {
   .on('finish', done)
 })
 
-gulp.task('test', function (done) {
+gulp.task('test', ['lint'], function (done) {
   gulp.src('./tests/connect-sequence.spec.js')
   .pipe(mocha())
   .on('finish', done)
 })
 
-gulp.task('default', sequence('lint', 'test'))
+gulp.task('bump', function () {
+  var opts = {}
+  opts.type = argv.patch
+    ? 'patch' : (argv.minor)
+      ? 'minor' : (argv.major)
+        ? 'major' : (argv.prerelease)
+          ? 'prerelease' : 'patch'
+  gulp.src('./package.json')
+  .pipe(bump(opts))
+  .pipe(gulp.dest('./'))
+})
+
+gulp.task('default', ['test'])
