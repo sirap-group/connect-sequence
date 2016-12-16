0afefa0bc59ffc882b7f0a585e92784e15c64774
Rémi Becheras <rbecheras@sirap.fr>
2016-12-16 02:06:43 +0100

    add a default task to run lint then test dev tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,21 @@
 var gulp = require('gulp')
 var linter = require('gulp-standard-bundle').linter
 var mocha = require('gulp-mocha')
+var sequence = require('run-sequence')
 
-gulp.task('lint', function () {
-  gulp.src(['./lib/**/*.js', './tests/**/*.spec.js'])
+gulp.task('lint', function (done) {
+  gulp.src(['./gulpfile.js', './lib/**/*.js', './tests/**/*.spec.js'])
   .pipe(linter())
   .pipe(linter.reporter('default', {
     breakOnError: true
   }))
+  .on('finish', done)
 })
 
-gulp.task('test', function () {
-  gulp.src('./tests/connect-sequence.spec.js').pipe(mocha())
+gulp.task('test', function (done) {
+  gulp.src('./tests/connect-sequence.spec.js')
+  .pipe(mocha())
+  .on('finish', done)
 })
+
+gulp.task('default', sequence('lint', 'test'))
